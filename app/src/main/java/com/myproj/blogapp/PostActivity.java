945f1c9ff5bec10f8a12c9054272541cff263928
Rust <m.a.islam@outlook.com>
2017-09-14 11:55:40 +0100

    rawr

--- app/src/main/java/com/myproj/blogapp/PostActivity.java
+++ app/src/main/java/com/myproj/blogapp/PostActivity.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.android.gms.nearby.connection.Strategy;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -36,6 +38,11 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 import jp.wasabeef.richeditor.RichEditor;
 
 public class PostActivity extends AppCompatActivity {
@@ -44,12 +51,14 @@ public class PostActivity extends AppCompatActivity {
     private EditText mPostTitle;
     private RichEditor mPostDesc;
 
+
     private TextView mPreview;
 
 
     private Button mSubmitBtn;
 
     private Uri mImageURi = null;
+    private Uri mcontentURI = null;
 
     private static final int GALLERY_REQUEST = 1;
 
@@ -67,62 +76,51 @@ public class PostActivity extends AppCompatActivity {
     private RichEditor mEditor;
     private RichEditor mEditordesc;
     private ScrollView ScrollView;
+    private String myURI;
+    public static   String c = "this string";
+    private  String newimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+
 /*        ScrollView ScrollView = (ScrollView) findViewById(R.id.scroll);
         ScrollView.fullScroll(ScrollView.FOCUS_DOWN);*/
 
 
         final ScrollView scrollview = ((ScrollView) findViewById(R.id.scroll));
+
+
         scrollview.postDelayed(new Runnable() {
             @Override
             public void run() {
                 scrollview.fullScroll(ScrollView.FOCUS_DOWN);
             }
-        },10000);
+        }, 10000);
 
-
-
-
-        View lastChild = scrollview.getChildAt(scrollview.getChildCount() - 1);
-        int bottom = lastChild.getBottom() + scrollview.getPaddingBottom();
-        int sy = scrollview.getScrollY();
-        int sh = scrollview.getHeight();
-        int delta = bottom - (sy + sh);
-
-        scrollview.smoothScrollBy(0, delta);
-
-        scrollview.fullScroll(View.FOCUS_DOWN);
-        scrollview.post(new Runnable() {
-            @Override
-
-            public void run() {
-
-                scrollview.scrollTo(0, scrollview.getBottom());
-                scrollview.fullScroll(View.FOCUS_DOWN);
-
-            }
-        });
 
         mEditor = (RichEditor) findViewById(R.id.editor);
         mEditordesc = (RichEditor) findViewById(R.id.descField);
 
         mEditordesc.setEditorHeight(18);
         mEditordesc.setEditorFontSize(18);
-        mEditordesc.setPadding(5,5,20,20);
+        mEditordesc.setPadding(5, 5, 20, 20);
 
         mEditordesc.setPlaceholder("Say something nice...      ");
 
 
         mEditor.setEditorHeight(18);
         mEditor.setEditorFontSize(18);
-        mEditor.setPadding(50,50,50,50);
+        mEditor.setPadding(50, 50, 50, 50);
         mEditor.setPlaceholder("Say something nice...      ");
         mEditor.loadCSS("file:///android_asset/img.css");
         mEditor.focusEditor();
+        mEditor.setKeepScreenOn(true);
+
+        scrollview.fullScroll(ScrollView.FOCUS_DOWN);
+        findViewById(R.id.editor).requestFocus();
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -137,9 +135,9 @@ public class PostActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mPostTitle = (EditText) findViewById(R.id.titleField);
-       // mPostDesc = (EditText) findViewById(R.id.descField);
+        // mPostDesc = (EditText) findViewById(R.id.descField);
         mPostDesc = mEditordesc;
-     //   mSubmitBtn = (Button) findViewById(R.id.btn);
+        //   mSubmitBtn = (Button) findViewById(R.id.btn);
 
 
         mProgress = new ProgressDialog(this);
@@ -166,8 +164,6 @@ public class PostActivity extends AppCompatActivity {
         });*/
 
 
-
-
         findViewById(R.id.action_bold1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -188,8 +184,25 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+        scrollview.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scrollview.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        }, 10000);
+    }
 
+    public static String random() {
+        Random generator = new Random();
 
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(32);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
     }
 
 
@@ -199,14 +212,57 @@ public class PostActivity extends AppCompatActivity {
         mProgress.getProgress();
         final String title_val = mPostTitle.getText().toString().trim();
         final String desc_val = mPostDesc.getHtml().toString().trim();
-        final String content_val = mEditor.getHtml();
 
+        final String content_val = mEditor.getHtml().toString();
+
+
+        String v = "";
+        Pattern pattern = Pattern.compile("src=\"(.*?)\"");
+        Matcher matcher = pattern.matcher(content_val);
+        int count = 0;
+
+        while (matcher.find()) {
+            count++;
+        }
+
+        final String countImg = Integer.toString(count);
+        if (matcher.find()) {
+            v = matcher.group(1);
+        }
+
+
+        mcontentURI = Uri.parse(countImg);
+final String img;
 
         if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageURi != null) {
             mProgress.show();
             StorageReference filepath = mStorage.child("Blog_Image").child(mImageURi.getLastPathSegment());
 
+
+            StorageReference filepath1 = mStorage.child("Blog_Image").child(mcontentURI.getLastPathSegment() + random());
+
+            filepath1.putFile(mcontentURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    final Uri downlaodUrl = taskSnapshot.getDownloadUrl();
+
+                    String b = downlaodUrl.toString();
+
+                    newimg  = b;
+
+                }
+
+            });
+
+
+
+
+
+
+
+
             filepath.putFile(mImageURi).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
 
 
                 @Override
@@ -221,7 +277,8 @@ public class PostActivity extends AppCompatActivity {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
 
-                            newPost.child("content").setValue(content_val);
+
+                            newPost.child("content").setValue(content_val + " " + newimg);
                             newPost.child("title").setValue(title_val);
                             newPost.child("desc").setValue(desc_val);
                             newPost.child("image").setValue(downlaodUrl.toString());
@@ -267,10 +324,9 @@ public class PostActivity extends AppCompatActivity {
             mImageURi = data.getData();
             mSelectImage.setImageURI(mImageURi);
 
-            String myURI = mImageURi.toString();
+            myURI = mImageURi.toString();
+
             mEditor.insertImage(myURI, "image");
-
-
 
 
         }
